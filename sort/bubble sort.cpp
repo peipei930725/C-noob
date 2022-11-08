e45215390493a9079e2a294b66a76c69ff762365
watermelon0725 <114333331+peipei930725@users.noreply.github.com>
2022-11-08 23:26:05 +0800

    Update bubble sort.cpp

--- sort/bubble sort.cpp
+++ sort/bubble sort.cpp
@@ -1,27 +1,22 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int swap(int a, int b){
-	if (a > b){
-		return (b, a);
-	}
-	return (a, b);
-}
-
-int main(){
+int main() {
 	const int arr_len = 5;
 	int arr[arr_len] = { 0 };
-	for (int i = 0; i < arr_len; ++i){
-		scanf_s("%d", arr[i]);
+	for (int i = 0; i < arr_len; ++i) {
+		scanf_s("%d", &arr[i]);
 	}
-	for (int i = 0; i < arr_len; ++i){
-		for (int j = 0; j < 5-i; ++j){
-			if (arr[j - 1]>arr[j]){
-				swap(arr[j - 1],arr[j]);
+	for (int i = 0; i < arr_len; ++i) {
+		for (int j = 1; j < 5 - i; ++j) {
+			if (arr[j-1] > arr[j]) {
+				int temp = arr[j-1];
+				arr[j-1] = arr[j];
+				arr[j] = temp;
 			}
 		}
 	}
-	for (int i = 0; i < arr_len; ++i){
+	for (int i = 0; i < arr_len; ++i) {
 		printf("%d  ", arr[i]);
 	}
 	system("pause");
